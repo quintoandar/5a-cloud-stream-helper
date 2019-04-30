97170200fc1b8b384d93d3cdf5f53d4fd760f4a4
lucaspg <lucas.goncalves@quintoandar.com.br>
2019-04-30 16:16:24 -0300

    Update gradle files and improve javadoc

--- src/main/java/br/com/quintoandar/cloudstream/streamlisteners/ChannelStreamListener.java
+++ src/main/java/br/com/quintoandar/cloudstream/streamlisteners/ChannelStreamListener.java
@@ -1,11 +1,17 @@
 package br.com.quintoandar.cloudstream.streamlisteners;
 
 public interface ChannelStreamListener<T> {
+    /**
+     * The first destination of a message. If this consumer fails to consume the message, it will be sent to retry expo
+     * topic.
+     * @param message the message that will be consumed
+     */
     void consume(T message);
 
     /**
      * The end destination of a message that have failed to be processed using the retry polices.
      * We will produce this message to a place where we can be informed that this happened.
-     **/
+     * @param message the message that will be consumed
+     */
     void dlq(T message);
 }
--- src/main/java/br/com/quintoandar/cloudstream/streamlisteners/ChannelStreamListenerRetryable.java
+++ src/main/java/br/com/quintoandar/cloudstream/streamlisteners/ChannelStreamListenerRetryable.java
@@ -5,12 +5,14 @@ public interface ChannelStreamListenerRetryable<T> extends ChannelStreamListener
     /**
      * Consumer that will handle the failed message and will retry exponentially to process the message
      * using the policies specified for the current channel input in the .properties file.
+     * @param message the message that will be consumed
      */
     void retryExpo(T message);
 
     /**
      * Consumer that will handle the failed message and will retry using linear time to process the message.
      * It will be called after retryExpo finished all attempts.
+     * @param message the message that will be consumed
      */
     void retryLinear(T message);
 }
